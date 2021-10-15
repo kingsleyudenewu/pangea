8ee90f68eb8b7529d28af0e01cbdac0e48b1c656
Kingsley Udenewu <kingzpacking@gmail.com>
2021-10-15 23:57:07 +0100

    Add json response and create topic and subscriber model and controllers

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,21 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Client\HttpClientException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Arr;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Tests\ApiResponse;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponse;
     /**
      * A list of the exception types that are not reported.
      *
@@ -34,8 +44,32 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (Throwable $e) {
+            $this->buildResponse($e);
         });
+    }
+
+    public function buildResponse(Throwable $e, $request = null)
+    {
+        if ($e instanceof ValidationException) {
+            return $this->formValidationErrorAlert('Validation error occurred', Arr::flatten($e->errors()));
+        }
+
+        if ($e instanceof ModelNotFoundException) {
+            return $this->notFoundResponse('Model cannot be found');
+        }
+        if ($e instanceof NotFoundHttpException) {
+            return $this->clientError($e->getMessage() || "Resource cannot be found", 404);
+        }
+        if ($e instanceof HttpResponseException) {
+            return $e->getResponse();
+        }
+        if ($e instanceof HttpException) {
+            return $this->jsonResponse($e->getMessage(), $e->getStatusCode());
+        }
+        if ($e instanceof HttpClientException) {
+            return $this->jsonResponse($e->getMessage(), $e->getStatusCode());
+        }
+        return $this->serverErrorAlert($e->getMessage());
     }
 }
