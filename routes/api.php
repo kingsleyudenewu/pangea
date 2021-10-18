e2a7acbd251fb8f995eaa2c2629b7fe054702412
Kingsley Udenewu <kingzpacking@gmail.com>
2021-10-18 10:50:24 +0100

    Add new endpoint to create subscriber and publish a message

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PublishController;
+use App\Http\Controllers\SubscriberController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/subscribe/{topic}', [SubscriberController::class, 'createSubscription']);
+Route::post('/publish/{topic}', [PublishController::class, 'publishMessage']);
