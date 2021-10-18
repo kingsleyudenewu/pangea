84d62ee4ab5e8b862b4e1b0f5e93b5b0cb114c12
Kingsley Udenewu <kingzpacking@gmail.com>
2021-10-18 14:50:35 +0100

    Revamp create subscribers

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -12,19 +12,10 @@ class SubscriberController extends Controller
 {
     public function createSubscription(CreateSubscriberRequest $request, string $topic): \Illuminate\Http\JsonResponse
     {
-        $createTopic = Topic::firstOrCreate(['name' => strtolower($topic)]);
-        $subscriber = Subscriber::firstOrCreate(['url' => strtolower($request->url)]);
+        $createTopic = Topic::updateOrCreate(['name' => strtolower($topic)]);
+        $subscriber = Subscriber::updateOrCreate(['url' => strtolower($request->url)]);
 
-        $subscription = Subscription::where('topic_id', $createTopic->id)
-            ->where('subscriber_id', $subscriber->id)
-            ->exists();
-
-        if (!$subscription) {
-            Subscription::create([
-                'topic_id' => $createTopic->id,
-                'subscriber_id' => $subscriber->id,
-            ]);
-        }
+        $createTopic->subscribers()->syncWithoutDetaching([$subscriber->id]);
 
         return response()->json([
             'url' => $subscriber->url,
